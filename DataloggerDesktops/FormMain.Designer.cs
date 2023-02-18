03f95a9250be15e0be5fce59fb2dfc05822f1684
VietNguyenXuan <nguyenviet.06092000@gmail.com>
2023-02-18 20:33:51 +0700

    Viet

--- DataloggerDesktops/FormMain.Designer.cs
+++ DataloggerDesktops/FormMain.Designer.cs
@@ -28,7 +28,25 @@
     /// </summary>
     private void InitializeComponent()
     {
+<<<<<<< Updated upstream
       this.components = new System.ComponentModel.Container();
+=======
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(500, 173);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(94, 29);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+>>>>>>> Stashed changes
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
       this.Text = "Form1";
