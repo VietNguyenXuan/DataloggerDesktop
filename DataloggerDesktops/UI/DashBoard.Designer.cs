224245fac9c543b78bc529c3f603c071be3a2786
VietNguyenXuan <nguyenviet.06092000@gmail.com>
2023-02-19 16:02:48 +0700

    19/02 Viet Lan 2

--- DataloggerDesktops/UI/DashBoard.Designer.cs
+++ DataloggerDesktops/UI/DashBoard.Designer.cs
@@ -34,12 +34,18 @@
       this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
       this.panelSpeech = new System.Windows.Forms.Panel();
       this.panelTemp = new System.Windows.Forms.Panel();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.panel2 = new System.Windows.Forms.Panel();
+      this.panel3 = new System.Windows.Forms.Panel();
       this.panelDashBoardFull.SuspendLayout();
       this.panelMon.SuspendLayout();
       this.SuspendLayout();
       // 
       // panelDashBoardFull
       // 
+      this.panelDashBoardFull.Controls.Add(this.panel3);
+      this.panelDashBoardFull.Controls.Add(this.panel2);
+      this.panelDashBoardFull.Controls.Add(this.panel1);
       this.panelDashBoardFull.Controls.Add(this.panelDevice);
       this.panelDashBoardFull.Controls.Add(this.panelMon);
       this.panelDashBoardFull.Controls.Add(this.panelSpeech);
@@ -85,6 +91,27 @@
       this.panelTemp.Size = new System.Drawing.Size(250, 177);
       this.panelTemp.TabIndex = 0;
       // 
+      // panel1
+      // 
+      this.panel1.Location = new System.Drawing.Point(36, 227);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(250, 177);
+      this.panel1.TabIndex = 4;
+      // 
+      // panel2
+      // 
+      this.panel2.Location = new System.Drawing.Point(316, 227);
+      this.panel2.Name = "panel2";
+      this.panel2.Size = new System.Drawing.Size(250, 177);
+      this.panel2.TabIndex = 4;
+      // 
+      // panel3
+      // 
+      this.panel3.Location = new System.Drawing.Point(592, 227);
+      this.panel3.Name = "panel3";
+      this.panel3.Size = new System.Drawing.Size(250, 177);
+      this.panel3.TabIndex = 5;
+      // 
       // DashBoard
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -107,5 +134,8 @@
     private System.Windows.Forms.MonthCalendar monthCalendar1;
     private System.Windows.Forms.Panel panelSpeech;
     private System.Windows.Forms.Panel panelTemp;
+    private System.Windows.Forms.Panel panel3;
+    private System.Windows.Forms.Panel panel2;
+    private System.Windows.Forms.Panel panel1;
   }
 }
