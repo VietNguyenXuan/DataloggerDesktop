5e43f737907c3abd9eced2d706fbefe593c2f984
huynhminh234 <hmminh51@gmal.com>
2023-02-19 22:52:44 +0700

    KJ

--- DataloggerDesktops/UI/SettingParametter.Designer.cs
+++ DataloggerDesktops/UI/SettingParametter.Designer.cs
@@ -29,46 +29,33 @@
     private void InitializeComponent()
     {
             this.label1 = new System.Windows.Forms.Label();
-<<<<<<< HEAD
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-=======
->>>>>>> main
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-<<<<<<< HEAD
             this.label1.Location = new System.Drawing.Point(452, 168);
-=======
-            this.label1.Location = new System.Drawing.Point(441, 206);
->>>>>>> main
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(147, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "SettingParameter";
             // 
-<<<<<<< HEAD
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(360, 250);
+            this.monthCalendar1.Location = new System.Drawing.Point(363, 246);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 2;
             // 
-=======
->>>>>>> main
             // SettingParametter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 562);
-<<<<<<< HEAD
             this.Controls.Add(this.monthCalendar1);
-=======
->>>>>>> main
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SettingParametter";
             this.Text = "SettingParametter";
             this.ResumeLayout(false);
