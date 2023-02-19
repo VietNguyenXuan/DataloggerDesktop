be7ec7c10bdcdec25b55b7f3610aa40c6481a47b
huynhminh234 <hmminh51@gmal.com>
2023-02-19 22:31:50 +0700

    LK

--- DataloggerDesktops/UI/SettingParametter.Designer.cs
+++ DataloggerDesktops/UI/SettingParametter.Designer.cs
@@ -28,33 +28,35 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.label1 = new System.Windows.Forms.Label();
-      this.SuspendLayout();
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(349, 215);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(123, 20);
-      this.label1.TabIndex = 1;
-      this.label1.Text = "SettingParameter";
-      // 
-      // SettingParametter
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Controls.Add(this.label1);
-      this.Name = "SettingParametter";
-      this.Text = "SettingParametter";
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(441, 206);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SettingParameter";
+            // 
+            // SettingParametter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SettingParametter";
+            this.Text = "SettingParametter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
     #endregion
 
     private System.Windows.Forms.Label label1;
-  }
+    }
 }
