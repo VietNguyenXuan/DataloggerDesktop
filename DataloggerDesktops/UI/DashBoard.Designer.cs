bea6c1b7c5c39d8ccea3b66ade8347706d2e2974
VietNguyenXuan <nguyenviet.06092000@gmail.com>
2023-02-19 21:17:54 +0700

    V

--- DataloggerDesktops/UI/DashBoard.Designer.cs
+++ DataloggerDesktops/UI/DashBoard.Designer.cs
@@ -29,6 +29,7 @@
     private void InitializeComponent()
     {
       this.panelDashBoardFull = new System.Windows.Forms.Panel();
+      this.button2 = new System.Windows.Forms.Button();
       this.button1 = new System.Windows.Forms.Button();
       this.panel3 = new System.Windows.Forms.Panel();
       this.panel2 = new System.Windows.Forms.Panel();
@@ -36,10 +37,16 @@
       this.panelDevice = new System.Windows.Forms.Panel();
       this.panelMon = new System.Windows.Forms.Panel();
       this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-      this.panelSpeech = new System.Windows.Forms.Panel();
       this.panelTemp = new System.Windows.Forms.Panel();
-      this.button2 = new System.Windows.Forms.Button();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.comboBox2 = new System.Windows.Forms.ComboBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.comboBox3 = new System.Windows.Forms.ComboBox();
+      this.label4 = new System.Windows.Forms.Label();
       this.panelDashBoardFull.SuspendLayout();
+      this.panelDevice.SuspendLayout();
       this.panelMon.SuspendLayout();
       this.SuspendLayout();
       // 
@@ -52,13 +59,21 @@
       this.panelDashBoardFull.Controls.Add(this.panel1);
       this.panelDashBoardFull.Controls.Add(this.panelDevice);
       this.panelDashBoardFull.Controls.Add(this.panelMon);
-      this.panelDashBoardFull.Controls.Add(this.panelSpeech);
       this.panelDashBoardFull.Controls.Add(this.panelTemp);
       this.panelDashBoardFull.Dock = System.Windows.Forms.DockStyle.Fill;
       this.panelDashBoardFull.Location = new System.Drawing.Point(0, 0);
       this.panelDashBoardFull.Name = "panelDashBoardFull";
       this.panelDashBoardFull.Size = new System.Drawing.Size(1148, 633);
       this.panelDashBoardFull.TabIndex = 1;
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(288, 438);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(564, 148);
+      this.button2.TabIndex = 7;
+      this.button2.Text = "Đức báo chúa";
+      this.button2.UseVisualStyleBackColor = true;
       // 
       // button1
       // 
@@ -92,9 +107,16 @@
       // 
       // panelDevice
       // 
+      this.panelDevice.Controls.Add(this.label4);
+      this.panelDevice.Controls.Add(this.label3);
+      this.panelDevice.Controls.Add(this.label2);
+      this.panelDevice.Controls.Add(this.comboBox3);
+      this.panelDevice.Controls.Add(this.comboBox2);
+      this.panelDevice.Controls.Add(this.label1);
+      this.panelDevice.Controls.Add(this.comboBox1);
       this.panelDevice.Location = new System.Drawing.Point(36, 23);
       this.panelDevice.Name = "panelDevice";
-      this.panelDevice.Size = new System.Drawing.Size(250, 177);
+      this.panelDevice.Size = new System.Drawing.Size(316, 177);
       this.panelDevice.TabIndex = 3;
       // 
       // panelMon
@@ -111,28 +133,76 @@
       this.monthCalendar1.Name = "monthCalendar1";
       this.monthCalendar1.TabIndex = 0;
       // 
-      // panelSpeech
-      // 
-      this.panelSpeech.Location = new System.Drawing.Point(592, 23);
-      this.panelSpeech.Name = "panelSpeech";
-      this.panelSpeech.Size = new System.Drawing.Size(250, 177);
-      this.panelSpeech.TabIndex = 1;
-      // 
       // panelTemp
       // 
-      this.panelTemp.Location = new System.Drawing.Point(316, 23);
+      this.panelTemp.Location = new System.Drawing.Point(494, 23);
       this.panelTemp.Name = "panelTemp";
-      this.panelTemp.Size = new System.Drawing.Size(250, 177);
+      this.panelTemp.Size = new System.Drawing.Size(221, 177);
       this.panelTemp.TabIndex = 0;
       // 
-      // button2
+      // comboBox1
       // 
-      this.button2.Location = new System.Drawing.Point(288, 438);
-      this.button2.Name = "button2";
-      this.button2.Size = new System.Drawing.Size(564, 148);
-      this.button2.TabIndex = 7;
-      this.button2.Text = "Đức báo chúa";
-      this.button2.UseVisualStyleBackColor = true;
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Location = new System.Drawing.Point(86, 42);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(215, 28);
+      this.comboBox1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label1.Location = new System.Drawing.Point(4, 42);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(76, 28);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Factory";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label2.Location = new System.Drawing.Point(3, 90);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(47, 28);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "Line";
+      // 
+      // comboBox2
+      // 
+      this.comboBox2.FormattingEnabled = true;
+      this.comboBox2.Location = new System.Drawing.Point(86, 90);
+      this.comboBox2.Name = "comboBox2";
+      this.comboBox2.Size = new System.Drawing.Size(215, 28);
+      this.comboBox2.TabIndex = 2;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label3.Location = new System.Drawing.Point(3, 136);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(70, 28);
+      this.label3.TabIndex = 9;
+      this.label3.Text = "Device";
+      // 
+      // comboBox3
+      // 
+      this.comboBox3.FormattingEnabled = true;
+      this.comboBox3.Location = new System.Drawing.Point(86, 136);
+      this.comboBox3.Name = "comboBox3";
+      this.comboBox3.Size = new System.Drawing.Size(215, 28);
+      this.comboBox3.TabIndex = 8;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.label4.Location = new System.Drawing.Point(3, 3);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(76, 28);
+      this.label4.TabIndex = 10;
+      this.label4.Text = "Device";
       // 
       // DashBoard
       // 
@@ -143,6 +213,8 @@
       this.Name = "DashBoard";
       this.Text = "FormDashBoard";
       this.panelDashBoardFull.ResumeLayout(false);
+      this.panelDevice.ResumeLayout(false);
+      this.panelDevice.PerformLayout();
       this.panelMon.ResumeLayout(false);
       this.ResumeLayout(false);
 
@@ -158,8 +230,14 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panelMon;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
-        private System.Windows.Forms.Panel panelSpeech;
         private System.Windows.Forms.Panel panelTemp;
     private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.ComboBox comboBox3;
+    private System.Windows.Forms.ComboBox comboBox2;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.ComboBox comboBox1;
   }
 }
